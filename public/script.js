8f1afd6168836426e27915ad89d4cbabd2e5845c
gladysyxn <xy25871@westridge.org>
2024-02-26 22:58:45 -0800

    initial
    initial

---
+++ public/script.js
@@ -0,0 +1,42 @@
+document.getElementById('searchInput').addEventListener('input', updateProducts);
+
+//priceValue
+async function updateProducts() {
+  try {
+    const name = document.getElementById('searchInput').value;
+
+    const response = await fetch('/api/products', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name }),
+    });
+
+    if (response.ok) {
+      const products = await response.json();
+      const container = document.getElementById('productsList');
+      container.innerHTML = '';
+      products.forEach(product => {
+        const productElement = `
+            <div>
+              <p><strong>${product.name}</strong></p>
+              <p>${product.description}</p>
+              <p>$${product.price}</p>
+              <p>${product.stock} available</p>
+              <p><img src="/images/${product.image}" height="200" " width="300"></p>
+              <button class="button">Add to Cart</button>
+
+        `;
+        container.innerHTML += productElement;
+      });
+    } else {
+      console.error('Response not ok with status:', response.status);
+    }
+  } catch (error) {
+    console.error('Fetch error:', error.message);
+  }
+}
+
+updateProducts();
+
